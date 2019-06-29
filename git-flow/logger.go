37741b600ebd1cf5dead24697e9d62351ba03df6
CJ Harries <cj@wizardsoftheweb.pro>
2019-06-29 14:34:21 -0500

    Throw in logrus

---
+++ git-flow/logger.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func BootstrapLogger() {
+	log.SetLevel(log.InfoLevel)
+	log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+	// Calls os.Exit(1) after logging
+	log.Fatal("Bye.")
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+}
