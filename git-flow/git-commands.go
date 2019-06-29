e221bfd512d00bfd7285510a22bd7440b0858107
CJ Harries <cj@wizardsoftheweb.pro>
2019-06-29 07:49:35 -0500

    Ensure repo is not bare

--- git-flow/git-commands.go
+++ git-flow/git-commands.go
@@ -3,6 +3,6 @@ package main
 import "gopkg.in/src-d/go-git.v4"
 
 func GitInit(repo_path string) error {
-	_, err := git.PlainInit(repo_path)
+	_, err := git.PlainInit(repo_path, false)
 	return err
 }
