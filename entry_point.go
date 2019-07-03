c9842d65198cd37fb11d48b4db6b0bcfced120c3
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-03 17:16:50 -0500

    Add flair to entry_point

--- entry_point.go
+++ entry_point.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/urfave/cli"
+)
+
+var (
+	VerbosityPattern = regexp.MustCompile("^-v+$")
+)
+
+var (
+	GlobalCliFlagVerbosity *cli.IntFlag
 )
 
 var (
@@ -20,7 +31,30 @@ var (
 	}
 )
 
-func BootstrapCli() *cli.App {
+func CheckVerbosity(args []string) ([]string, int) {
+	sanitized_args := []string{}
+	verbosity_flag := ""
+	for _, arg := range os.Args {
+		if VerbosityPattern.MatchString(arg) {
+			verbosity_flag = fmt.Sprintf("%s%s", verbosity_flag, arg)
+		} else {
+			sanitized_args = append(sanitized_args, arg)
+		}
+	}
+	return sanitized_args, strings.Count(verbosity_flag, "v")
+}
+
+func PopulateContext(context *cli.Context) error {
+	BootstrapLogger(context.Int("verbose"))
+	return nil
+}
+
+func BootstrapCli(verbosity_level int) *cli.App {
+	GlobalCliFlagVerbosity = &cli.IntFlag{
+		Name:  "verbose, v",
+		Usage: "The more vs the more verbose the logs",
+		Value: verbosity_level,
+	}
 	app := cli.NewApp()
 	app.Name = "git-flow"
 	app.Version = GITFLOW_VERSION
@@ -34,13 +68,17 @@ func BootstrapCli() *cli.App {
 		CommandSupport,
 		CommandVersion,
 	}
-	app.Flags = []cli.Flag{}
+	app.Flags = []cli.Flag{
+		GlobalCliFlagVerbosity,
+	}
+	app.Before = PopulateContext
 	return app
 }
 
 func main() {
-	app := BootstrapCli()
-	err := app.Run(os.Args)
+	sanitized_args, verbosity_level := CheckVerbosity(os.Args)
+	app := BootstrapCli(verbosity_level)
+	err := app.Run(sanitized_args)
 	if nil != err {
 		log.Fatal(err)
 	}
