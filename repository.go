11b58eb013dee9581c5ae8c29aeba0c4c099cc62
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-03 08:02:21 -0500

    Create logic to discover repo

--- repository.go
+++ repository.go
@@ -1,16 +1,42 @@
 package main
 
-import "path/filepath"
+import (
+	"errors"
+	"path/filepath"
+)
+
+var (
+	ErrNotARepo = errors.New("Not a repo")
+)
 
 type Repository struct {
 	dotDir FileSystemObject
 }
 
-func (repo *Repository) LoadOrInit(directory string) error {
-	var err error
-	repo.dotDir, err = FileSystemObject(filepath.Join(directory, ".git")).ClimbUpwardsToFind(".git")
-	if nil != err {
-		println("Must init")
+func (repo Repository) discoverDotDir(root FileSystemObject) (FileSystemObject, error) {
+	if root.isRoot() {
+		return root, ErrNotARepo
 	}
+	result, err := root.DirectoryContains(".git")
+	if nil != err {
+		return root, err
+	} else if result {
+		return FileSystemObject(filepath.Join(root.String(), ".git")), nil
+	}
+	return repo.discoverDotDir(root.Parent())
+}
+
+func (repo *Repository) LoadOrInit(directory string) error {
+	dot_dir, err := repo.discoverDotDir(FileSystemObject(directory))
+	if nil != err {
+		if ErrNotARepo == err {
+			println("must init")
+		} else {
+			return err
+		}
+	} else {
+		println("cool")
+	}
+	repo.dotDir = dot_dir
 	return nil
 }
