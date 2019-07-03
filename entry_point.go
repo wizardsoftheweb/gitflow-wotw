3441c657c965df0611640cb76a6415ea6070d6de
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-03 08:57:30 -0500

    Add BootstrapCli

--- entry_point.go
+++ entry_point.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"log"
 	"os"
+	"time"
+
+	"github.com/urfave/cli"
 )
 
 var (
 	repo Repository
 )
 
+func BootstrapCli() *cli.App {
+	app := cli.NewApp()
+	app.Name = "git-flow"
+	app.Compiled = time.Now()
+	app.Commands = []cli.Command{}
+	app.Flags = []cli.Flag{}
+	return app
+}
+
 func main() {
-	cwd, _ := os.Getwd()
-	repo.LoadOrInit(cwd)
-	// path := FileSystemObject(cwd)
-	// path.SearchDirectoryAbove("go-git2")
+	app := BootstrapCli()
+	err := app.Run(os.Args[1:])
+	if nil != err {
+		log.Fatal(err)
+	}
 }
