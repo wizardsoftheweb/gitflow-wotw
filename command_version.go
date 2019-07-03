670f3a09cc4374453f1d1e0d64fc40d1fc4eb92d
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-03 09:48:53 -0500

    Define version command

--- command_version.go
+++ command_version.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+const (
+	defaultGitFlowRootPackage = "gitflow"
+	defaultGitFlowRootVersion = "0.4.2-pre"
+	defaultGitFlowRootUrl     = "https://github.com/nvie/gitflow"
+)
 
 var (
 	CommandVersion = cli.Command{
@@ -11,5 +21,13 @@ var (
 )
 
 func CommandVersionAction(context *cli.Context) error {
+	fmt.Fprintf(
+		context.App.Writer,
+		"Version %s of gitflow-wotw was based on the following work:\n\tPackage: %s\n\tVersion: %s\n\tUrl: %s",
+		context.App.Version,
+		defaultGitFlowRootPackage,
+		defaultGitFlowRootVersion,
+		defaultGitFlowRootUrl,
+	)
 	return nil
 }
