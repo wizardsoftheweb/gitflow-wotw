0de7ba0c58b12b997980ef7c66e52d3a08d5a89f
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-04 12:41:10 -0500

    Convert the subcmd fncs to actions (whoops)

--- command_feature.go
+++ command_feature.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
@@ -27,45 +29,47 @@ var (
 
 func BeforeFeature(context *cli.Context) error {
 	Repo.Prefix = GitConfig.GetWithDefault(FEATURE_PREFIX_KEY, DefaultPrefixFeature.Value)
+	Repo.HumanPrefix = strings.TrimSuffix(Repo.Prefix, "/")
 }
 
 func CommandFeatureAction(context *cli.Context) error {
 	logrus.Debug("CommandFeatureAction")
 	return nil
 }
-func CommandFeatureList(context *cli.Context) error {
-	logrus.Debug("CommandFeatureList")
+func CommandFeatureListAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureListAction")
+
 	return nil
 }
-func CommandFeatureStart(context *cli.Context) error {
-	logrus.Debug("CommandFeatureStart")
+func CommandFeatureStartAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureStartAction")
 	return nil
 }
-func CommandFeatureFinish(context *cli.Context) error {
-	logrus.Debug("CommandFeatureFinish")
+func CommandFeatureFinishAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureFinishAction")
 	return nil
 }
-func CommandFeaturePublish(context *cli.Context) error {
-	logrus.Debug("CommandFeaturePublish")
+func CommandFeaturePublishAction(context *cli.Context) error {
+	logrus.Debug("CommandFeaturePublishAction")
 	return nil
 }
-func CommandFeatureTrack(context *cli.Context) error {
-	logrus.Debug("CommandFeatureTrack")
+func CommandFeatureTrackAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureTrackAction")
 	return nil
 }
-func CommandFeatureDiff(context *cli.Context) error {
-	logrus.Debug("CommandFeatureDiff")
+func CommandFeatureDiffAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureDiffAction")
 	return nil
 }
-func CommandFeatureRebase(context *cli.Context) error {
-	logrus.Debug("CommandFeatureRebase")
+func CommandFeatureRebaseAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureRebaseAction")
 	return nil
 }
-func CommandFeatureCheckout(context *cli.Context) error {
-	logrus.Debug("CommandFeatureCheckout")
+func CommandFeatureCheckoutAction(context *cli.Context) error {
+	logrus.Debug("CommandFeatureCheckoutAction")
 	return nil
 }
-func CommandFeaturePull(context *cli.Context) error {
-	logrus.Debug("CommandFeaturePull")
+func CommandFeaturePullAction(context *cli.Context) error {
+	logrus.Debug("CommandFeaturePullAction")
 	return nil
 }
