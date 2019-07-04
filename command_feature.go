c8e9d6d252e050dfcf8198db2159a72f50bc5325
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-04 12:43:44 -0500

    Add full subcommand defs

--- command_feature.go
+++ command_feature.go
@@ -8,6 +8,51 @@ import (
 )
 
 var (
+	CommandFeatureList = cli.Command{
+		Name:   "list",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureListAction,
+	}
+	CommandFeatureStart = cli.Command{
+		Name:   "start",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureStartAction,
+	}
+	CommandFeatureFinish = cli.Command{
+		Name:   "finish",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureFinishAction,
+	}
+	CommandFeaturePublish = cli.Command{
+		Name:   "publish",
+		Flags:  []cli.Flag{},
+		Action: CommandFeaturePublishAction,
+	}
+	CommandFeatureTrack = cli.Command{
+		Name:   "track",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureTrackAction,
+	}
+	CommandFeatureDiff = cli.Command{
+		Name:   "diff",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureDiffAction,
+	}
+	CommandFeatureRebase = cli.Command{
+		Name:   "rebase",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureRebaseAction,
+	}
+	CommandFeatureCheckout = cli.Command{
+		Name:   "checkout",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureCheckoutAction,
+	}
+	CommandFeaturePull = cli.Command{
+		Name:   "pull",
+		Flags:  []cli.Flag{},
+		Action: CommandFeaturePullAction,
+	}
 	CommandFeature = cli.Command{
 		Name:   "feature",
 		Flags:  []cli.Flag{},
