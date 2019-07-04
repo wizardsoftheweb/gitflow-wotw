53f40ee768474bed7db30319ef14588a14279b17
CJ Harries <cj@wizardsoftheweb.pro>
2019-07-04 03:32:42 -0500

    Add back some old stuff

---
+++ command_feature.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+var (
+	CommandFeature = cli.Command{
+		Name:   "feature",
+		Flags:  []cli.Flag{},
+		Action: CommandFeatureAction,
+	}
+)
+
+func CommandFeatureAction(context *cli.Context) error {
+	logrus.Trace("CommandFeatureAction")
+	TestRun()
+	return nil
+}
